9fa9f3529177ef0109d573970227d3f68d00dcab
tency <tencygjq@163.com>
2021-04-17 13:26:53 +0800

    完善测试用例

--- test/string_filter_test.go
+++ test/string_filter_test.go
@@ -11,7 +11,7 @@ func TestStringFilter(t *testing.T) {
 	fmt.Println(">>>>> StringFilter test begin")
 
 	filter := utils.NewStringFilter()
-	before := "goo Dxxxg afuck god hell wwwankerww bunny fucker"
+	before := "goo Dxxxg afuck god hello Hello wwwankerww bunny fucker"
 	after := filter.Replace(before)
 	fmt.Printf("before replace: %s\n", before)
 	fmt.Printf(" after replace: %s\n", after)
